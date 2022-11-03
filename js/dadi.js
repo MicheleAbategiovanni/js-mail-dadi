4489c9e877a183e7b72d83ad4367c76574749cdf
MicheleAbategiovanni <michele.abategiovanni@gmail.com>
2022-11-04 00:52:20 +0100

    complete dadi coding

--- js/dadi.js
+++ js/dadi.js
@@ -1,19 +1,42 @@
 let numberUser = Math.floor(Math.random() * 6) + 1;
 let numberCPU = Math.floor(Math.random() * 6) + 1;
 
+const userEl = document.querySelector("#number_user");
+const cpuEl = document.querySelector("#number_cpu");
+const game = document.querySelector(".ciao");
+const resultEl = document.querySelector("h1");
+
+
 console.log("Numero utente: " + numberUser);
 
 console.log("Numero CPU: " + numberCPU);
 
 
-if (numberCPU > numberUser) {
+game.addEventListener("click", function () {
 
-    alert("Il Banco Vince... Riprova sarai più fortunato")  
-} else if (numberCPU < numberUser) {
+    userEl.innerHTML = numberUser
 
-    alert("Hai vinto congratulazioni !!!");
+    cpuEl.innerHTML = numberCPU
 
-} else if (numberCPU === numberUser){
+    if (numberCPU > numberUser) {
 
-    alert("Tu ed il banco avete gli stessi numeri...");
-}
+        // alert("Il Banco Vince... Riprova sarai più fortunato") 
+
+        resultEl.innerHTML = ("Il banco vince")
+
+    } else if (numberCPU < numberUser) {
+
+        resultEl.innerHTML = ("Hai vinto !!")
+
+        // alert("Hai vinto congratulazioni !!!");
+
+    } else if (numberCPU === numberUser) {
+
+        resultEl.innerHTML = ("Pareggio")
+
+        // alert("Tu ed il banco avete gli stessi numeri...");
+    }
+
+})
+
+
